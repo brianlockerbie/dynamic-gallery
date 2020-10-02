aac1df4cfe0ee61dba71fe49bd3997a59df2d299
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-10-02 14:40:28 -0400

    Added random photod and buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [images, setImages] = useState([
+    {url: "https://source.unsplash.com/random/1"},
+    {url: "https://source.unsplash.com/random/2"},
+    {url: "https://source.unsplash.com/random/3"},
+    {url: "https://source.unsplash.com/random/4"},
+    {url: "https://source.unsplash.com/random/5"},
+    {url: "https://source.unsplash.com/random/6"}
+  ])
+
   return (
     <div className="App">
-      
+      <div className="cardContainer">
+        
+      </div>
+      <div className="btnContainer">
+        <button>Remove</button>
+        <button>Add</button>
+      </div>
     </div>
   );
 }
